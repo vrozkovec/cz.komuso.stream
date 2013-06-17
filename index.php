3245e12d85836881414e82f0402da6ef96da91ef
vrozkovec <vit.rozkovec@gmail.com>
2013-06-17 20:49:49 +0200

    changed ports

--- index.php
+++ index.php
@@ -127,7 +127,7 @@
 				  
 			  function komusoPlayer(id, stream){
 					jwplayer(id).setup({
-						file: "rtmp://ks380544.kimsufi.comi/oflaDemo/flv:"+stream,
+						file: "rtmp://ks380544.kimsufi.com/oflaDemo/flv:"+stream,
 						primary: "flash",
 						autostart: true
 					});
