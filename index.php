43c9f0c4a2534a893e21449764a9602fb2f130dc
vrozkovec <vit.rozkovec@gmail.com>
2013-06-17 12:10:08 +0200

    8080

--- index.php
+++ index.php
@@ -127,7 +127,7 @@
 				  
 			  function komusoPlayer(id, stream){
 					jwplayer(id).setup({
-						file: "rtmp://ks380544.kimsufi.com/oflaDemo/flv:"+stream,
+						file: "rtmp://ks380544.kimsufi.comi:8080/oflaDemo/flv:"+stream,
 						primary: "flash",
 						autostart: true
 					});
